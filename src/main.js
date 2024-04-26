c07248e91fd421829a5437221d71e4a7631fe071
SmBush420 <olegsacuk9@gmail.com>
2024-04-26 15:16:01 +0300

    new repository

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "@/assets/index.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import { createPinia } from "pinia";
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount("#app");
---
+++ src/store/general.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const useGeneralStore = defineStore("general", {
+  state: () => ({
+    loading: false,
+    error: null,
+  }),
+  getters: {},
+  actions: {
+    setError(error) {
+      this.error = error;
+    },
+    setLoading(value) {
+      this.loading = value;
+    },
+  },
+});
